362a9c0f2649240656b33ef88aa3b903690acd0c
anoceto <agusnoceto@gmail.com>
2023-10-20 13:55:33 -0300

    dynamic rules

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/agusnoceto/modak-challenge/internal/model"
 	"github.com/agusnoceto/modak-challenge/internal/notification"
 	"github.com/agusnoceto/modak-challenge/ui"
+	"os"
 	"time"
 )
 
@@ -12,11 +14,11 @@ func main() {
 	gateway := notification.NewGateway()
 	repository := notification.NewVolatileRepository()
 
-	rules := map[model.MessageKey]model.RateRule{}
-	rules[model.MessageKeyStatus] = model.RateRule{Interval: time.Minute, Limit: 2}
-	rules[model.MessageKeyNews] = model.RateRule{Interval: 24 * time.Hour, Limit: 1}
-	rules[model.MessageKeyMarketing] = model.RateRule{Interval: time.Hour, Limit: 3}
-
+	rules, err := parseRulesJSONFile("rules.json")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	limiter := notification.NewRateLimiter(rules, repository)
 	service := notification.NewRateLimitingService(repository, *gateway, limiter)
 
@@ -27,7 +29,7 @@ func main() {
 	for keepSending {
 		ui.PrintDelimiter()
 
-		key, email, msg := ui.ReadValues()
+		key, email, msg := ui.ReadValues(rules)
 		err := service.Send(key, email, msg)
 		if err != nil {
 			fmt.Println(fmt.Sprintf("Error: %s", err))
@@ -35,4 +37,39 @@ func main() {
 		keepSending = ui.SendAnotherMessage()
 	}
 	ui.PrintGoodBye()
+}
+
+func parseRulesJSONFile(filename string) (map[model.MessageKey]model.RateRule, error) {
+	type tempRule struct {
+		Name     string
+		Interval string
+		Limit    int
+	}
+
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var rules []tempRule
+
+	if err := json.Unmarshal(content, &rules); err != nil {
+		return nil, err
+	}
+
+	ruleMap := make(map[model.MessageKey]model.RateRule)
+	for _, rule := range rules {
+		interval, err := time.ParseDuration(rule.Interval)
+		if err != nil {
+			return nil, err
+		}
+
+		ruleMap[model.MessageKey(rule.Name)] = model.RateRule{
+			Name:     rule.Name,
+			Interval: interval,
+			Limit:    rule.Limit,
+		}
+	}
+
+	return ruleMap, nil
 }
